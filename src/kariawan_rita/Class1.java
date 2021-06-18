9c894fcab50d914a46333223d85afd5f3a84ba4c
Ritapurnamasarii <ritapurnamasarii02@gmail.com>
2021-06-18 19:34:35 +0800

    Pembuatan Class3 untuk pembuatan Objek dari Class1 dan Class2

--- src/kariawan_rita/Class1.java
+++ src/kariawan_rita/Class1.java
@@ -10,32 +10,15 @@ public class Class1 {
     public void Alamat(){
         System.out.println("Alamat kariawan "+nama+" bertempat di "+alamat);
     }
-
     public void Desc(){
         System.out.println("Kariawan "+id+" dengan nama lengkap "+nama+" lahir pada tanggal "+tanggal_lahir+" dengan kelamin "+gender );
     }
     public void Gajih(){
         gajih = masuk * 800000;
-        System.out.println("kariawan "+nama+" mendapatkan gajih sebesar Rp."+gajih);
+        System.out.println("kariawan "+nama+" mendapatkan gajih sebesar Rp "+gajih);
     }
     public void infoAbsen(){
         absen = 20 - masuk;
         System.out.println(nama+" absen "+absen+" hari pada bulan ini");
-    }
-
-    public static void main(String[] args) {
-        Class1 Rita = new Class1();
-        Rita.id = "001";
-        Rita.nama = "Rita Purnamasari";
-        Rita.alamat = "Bangkal";
-        Rita.tanggal_lahir = "05 September 2001";
-        Rita.gender = "Perempuan";
-        Rita.masuk = 19;
-
-        Rita.Desc();
-        Rita.Alamat();
-        Rita.Gajih();
-        Rita.infoAbsen();
-
     }
 }
--- src/kontrak_kariawan_rita/Class2.java
+++ src/kontrak_kariawan_rita/Class2.java
@@ -15,23 +15,4 @@ public class Class2 extends Class1 {
         System.out.println("Masa Kerja "+nama+" akan berakhir pada tahun "+(join + kontrak));
     }
 
-    public static void main(String[] args) {
-        Class2 Resa = new Class2();
-        Resa.id = "002";
-        Resa.nama = "Resa Damayanti";
-        Resa.alamat = "Bangkal Banjarbaru";
-        Resa.tanggal_lahir = "19 Desember 2004";
-        Resa.gender = "Perempuan";
-        Resa.masuk = 15;
-        Resa.bidang = "Busana Butik";
-        Resa.join = 2019;
-        Resa.kontrak = 5;
-
-        Resa.Desc();
-        Resa.Alamat();
-        Resa.Gajih();
-        Resa.infoAbsen();
-        Resa.Bidang();
-        Resa.masaKerja();
-    }
 }
